62a437b68784f0b671974e5a0064857f2cb412bb
Ben Skeggs <bskeggs@redhat.com>
2018-05-08 20:39:47 +1000

    gr/gf100-: virtualise r419e00
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- drm/nouveau/nvkm/engine/gr/ctxgf100.c
+++ drm/nouveau/nvkm/engine/gr/ctxgf100.c
@@ -1429,6 +1429,8 @@ gf100_grctx_generate_main(struct gf100_gr *gr, struct gf100_grctx *info)
 		grctx->r418800(gr);
 	if (grctx->r419eb0)
 		grctx->r419eb0(gr);
+	if (grctx->r419e00)
+		grctx->r419e00(gr);
 }
 
 #define CB_RESERVED 0x80000
